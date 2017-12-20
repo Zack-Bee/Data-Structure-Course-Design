1bdadc23b659959690c87934b9fea4ab2f283d3d
tuhao <1033254071@qq.com>
2017-12-20 16:06:26 +0800

    sds

--- src/contacts/tinyServer.c
+++ src/contacts/tinyServer.c
@@ -1,3 +1,8 @@
+/**
+ *     此模块参考了"喜欢兰花山丘"的实现, blog:
+ *     http://www.cnblogs.com/life2refuel/p/5277111.html
+ */
+
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
@@ -11,6 +16,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#include "sds.h"
 
 #define SERVER_PORT 3000
 #define isSpace(c) ((c == ' ') || (c >= '\t' && c <= '\r'))
@@ -34,6 +41,7 @@ int getFileDescLine(int fileDesc, char buf[], int size);
  *     返回400 请求解析失败, 客户端代码错误
  *     @param clientFileDesc 客户端文件描述符
  */
+
 inline void response_400(int clientFileDesc);
 
 /**
@@ -119,100 +127,94 @@ int main() {
     return 0;
 }
 
-int getFileDescLine(int fileDesc, char buf[], int size) {
-    char *tp = buf;
-    char c;
 
-    --size;
-    while ((tp - buf) < size) {
-        if (read(fileDesc, &c, 1) <= 0) //伪造结束条件
-            break;
-        if (c == '\r') { //全部以\r分割
-            if (recv(fileDesc, &c, 1, MSG_PEEK) > 0 && c == '\n')
-                read(fileDesc, &c, 1);
-            else //意外的结束,填充 \n 结束读取
-                *tp++ = '\n';
+int getFileDescLine(int fileDesc, char buf[], int size) {
+    char *ptr = buf;
+    char ch;
+    size--;
+    while ((ptr - buf) < size) {
+
+        // 没有读到字符则退出
+        if (read(fileDesc, &ch, 1) <= 0) {
             break;
         }
-        *tp++ = c;
+
+        // 读取http header
+        if (ch == '\r') {
+            if (recv(fileDesc, &ch, 1, MSG_PEEK) > 0 && ch == '\n') {
+                read(fileDesc, &ch, 1);
+            } else { //意外的结束,填充 \n 结束读取
+                *ptr++ = '\n';
+            }
+            break;
+        }
+        *ptr++ = ch;
     }
-    *tp = '\0';
-    return tp - buf;
+    *ptr = '\0';
+    return ptr - buf;
 }
 
-// 返回400 请求解析失败,客户端代码错误
 inline void response_400(int clientFileDesc) {
-    const char *estr = "HTTP/1.0 400 BAD REQUEST\r\n"
-                       "Server: wz simple httpd 1.0\r\n"
-                       "Content-Type: text/html\r\n"
+    const char *estr = "HTTP/1.1 400 BAD REQUEST\r\n"
+                       "Server: zackbee'server 1.0\r\n"
+                       "Content-Type: text/html; charset=UTF-8\r\n"
                        "\r\n"
-                       "<p>你的请求有问题,请检查语法!</p>\r\n";
+                       "<p>请检查语法!</p>\r\n";
 
     write(clientFileDesc, estr, strlen(estr));
 }
 
-// 返回404 文件内容, 请求文件没有找见
 inline void response_404(int clientFileDesc) {
-    const char *estr = "HTTP/1.0 404 NOT FOUND\r\n"
-                       "Server: wz simple httpd 1.0\r\n"
-                       "Content-Type: text/html\r\n"
+    const char *estr = "HTTP/1.1 404 NOT FOUND\r\n"
+                       "Server: zackbee'server 1.0\r\n"
+                       "Content-Type: text/html; charset=UTF-8\r\n"
                        "\r\n"
                        "<html>"
-                       "<head><title>你请求的界面被查水表了!</title></head>\r\n"
-                       "<body><p>404: 估计是回不来了</p></body>"
+                       "<head><title>404 NOT FOUND</title></head>\r\n"
+                       "<body><p>404: 你请求的页面不存在</p></body>"
                        "</html>";
 
-    //开始发送数据
     write(clientFileDesc, estr, strlen(estr));
 }
 
-// 返回501 错误, 请求解析失败,不支持的请求
 inline void response_501(int clientFileDesc) {
-    const char *estr = "HTTP/1.0 501 Method Not Implemented\r\n"
-                       "Server: wz simple httpd 1.0\r\n"
-                       "Content-Type: text/html\r\n"
-                       "\r\n"
-                       "<html>"
-                       "<head><title>小伙子不要乱请求</title></head>\r\n"
-                       "<body><p>too young too simple, "
-                       "年轻人别总想弄出个大新闻.</p></body>"
-                       "</html>";
+    const char *estr =
+            "HTTP/1.1 501 Method Not Implemented\r\n"
+            "Server: zackbee'server 1.0\r\n"
+            "Content-Type: text/html; charset=UTF-8\r\n"
+            "\r\n"
+            "<html>"
+            "<head><title>501 Method Not Implemented</title></head>\r\n"
+            "<body><p>501: Method Not Implemented</p></body>"
+            "</html>";
 
-    //这里还有一个好的做法是将这些内容定义在文件中输出文件
     write(clientFileDesc, estr, strlen(estr));
 }
 
-
-// 服务器内部错误,无法处理等
 inline void response_500(int clientFileDesc) {
-    const char *estr = "HTTP/1.0 500 Internal Server Error\r\n"
-                       "Server: wz simple httpd 1.0\r\n"
-                       "Content-Type: text/html\r\n"
-                       "\r\n"
-                       "<html>"
-                       "<head><title>Sorry </title></head>\r\n"
-                       "<body><p>最近有点方了!</p></body>"
-                       "</html>";
+    const char *estr =
+            "HTTP/1.1 500 Internal Server Error\r\n"
+            "Server: zackbee'server 1.0\r\n"
+            "Content-Type: text/html; charset=UTF-8\r\n"
+            "\r\n"
+            "<html>"
+            "<head><title>500 Internal Server Error</title></head>\r\n"
+            "<body><p>500: Internal Server Error</p></body>"
+            "</html>";
 
     write(clientFileDesc, estr, strlen(estr));
 }
 
-// 返回200 请求成功 内容, 后面可以加上其它参数,处理文件输出
-inline void response_200(int clientFileDesc) {
-    // 打印返回200的报文头
-    const char *str = "HTTP/1.0 200 OK\r\n"
-                      "Server: wz simple httpd 1.0\r\n"
-                      "Content-Type: text/html\r\n"
-                      "\r\n";
-
-    write(clientFileDesc, str, strlen(str));
+inline void response_200(int clientFileDesc, const char *type) {
+    char templete[BUFF_SIZE] = "HTTP/1.1 200 OK\r\n"
+                               "Server: zackbee'server 1.0\r\n"
+                               "Content-Type: %s; charset=UTF-8\r\n"
+                               "\r\n";
+    char header[BUFFER_SIZE];
+    sprintf(header, templete, type);
+    write(clientFileDesc, header, strlen(header));
 }
 
-/*
- * 将文件 发送给客户端
- * clientFileDesc        : 客户端文件描述符
- * path        : 发送的文件路径
- */
 void responseFile(int clientFileDesc, const char *path) {
     FILE *txt;
     char buf[BUFF_SIZE];
@@ -222,7 +224,7 @@ void responseFile(int clientFileDesc, const char *path) {
            strcmp("\n", buf))
         ;
     // 这里开始处理 文件内容
-    if ((txt = fopen(path, "r")) == NULL) //文件解析错误,给它个404
+    if ((txt = fopen(path, "r")) == NULL) //文件解析错误, 返回404
         response_404(clientFileDesc);
     else {
         response_200(clientFileDesc); //发送给200的报文头过去
@@ -288,9 +290,11 @@ void *responseClient(int *clientFileDesc) {
     // if ((st.st_mode & S_IXUSR) || (st.st_mode & S_IXGRP) ||
     // (st.st_mode & S_IXOTH))
     // iscgi = 0;
+
+    // 处理GET请求
     if (strcasecmp("GET", path) == 0) {
         responseFile(*clientFileDesc, path);
-    } else {
+    } else { // 处理post请求
         responseMessage(*clientFileDesc, path, method);
     }
     close(*clientFileDesc);
@@ -304,6 +308,9 @@ void responseMessage(int clientFileDesc, const char *path, const char *method) {
     int contlen = -1; //报文长度
     char c;
 
+    // 处理POST, 在浏览器实现post操作时, 会用tcp发送两个package, 一个是http
+    // header, 一个是http body, 所以这个地方再次读取了从服务器发送过来的数据,
+    // 即http body
     if (strcasecmp(method, "POST") == 0) {
         while (getFileDescLine(clientFileDesc, buf, sizeof buf) > 0 &&
                strcmp("\n", buf)) {
@@ -316,72 +323,73 @@ void responseMessage(int clientFileDesc, const char *path, const char *method) {
             response_400(clientFileDesc);
             return;
         }
-    } else { // 读取报文头,就是过滤, 后面就假定是 GET
-        while (getFileDescLine(clientFileDesc, buf, sizeof buf) > 0 &&
-               strcmp("\n", buf)) {
-        };
     }
+    //  else { // 读取报文头,就是过滤, 后面就假定是 GET
+    //     while (getFileDescLine(clientFileDesc, buf, sizeof buf) > 0 &&
+    //            strcmp("\n", buf)) {
+    //     };
+    // }
 
-    //这里处理请求内容, 先处理错误信息
-    if (pipe(pocgi) < 0) {
-        response_500(clientFileDesc);
-        return;
-    }
+    // //这里处理请求内容, 先处理错误信息
+    // if (pipe(pocgi) < 0) {
+    //     response_500(clientFileDesc);
+    //     return;
+    // }
 
-    //管道, 0读取, 1写入
-    if (pipe(picgi) < 0) {
-        close(pocgi[0]), close(pocgi[1]);
-        response_500(clientFileDesc);
-        return;
-    }
-    if ((pid = fork()) < 0) {
-        close(pocgi[0]), close(pocgi[1]);
-        close(picgi[0]), close(picgi[1]);
-        response_500(clientFileDesc);
-        return;
-    }
+    // //管道, 0读取, 1写入
+    // if (pipe(picgi) < 0) {
+    //     close(pocgi[0]), close(pocgi[1]);
+    //     response_500(clientFileDesc);
+    //     return;
+    // }
+    // if ((pid = fork()) < 0) {
+    //     close(pocgi[0]), close(pocgi[1]);
+    //     close(picgi[0]), close(picgi[1]);
+    //     response_500(clientFileDesc);
+    //     return;
+    // }
 
-    // 这里就是多进程处理了, 先处理子进程
-    if (pid == 0) {
-        // dup2 让 前者共享后者同样的文件表
-        dup2(pocgi[1], STDOUT_FILENO); //标准输出算作 pocgi管道 的写入端
-        dup2(picgi[0], STDIN_FILENO); //标准输入做为picgif管道的读取端
-        close(pocgi[0]);
-        close(pocgi[1]);
+    // // 这里就是多进程处理了, 先处理子进程
+    // if (pid == 0) {
+    //     // dup2 让 前者共享后者同样的文件表
+    //     dup2(pocgi[1], STDOUT_FILENO); //标准输出算作 pocgi管道 的写入端
+    //     dup2(picgi[0], STDIN_FILENO); //标准输入做为picgif管道的读取端
+    //     close(pocgi[0]);
+    //     close(pocgi[1]);
 
-        // 添加环境变量,用于当前会话中
-        sprintf(buf, "REQUEST_METHOD=%s", method);
-        putenv(buf);
-        // 继续凑环境变量串,放到当前会话种
-        if (strcasecmp(buf, "POST") == 0) {
-            sprintf(buf, "CONTENT_LENGTH=%d", contlen);
-        }
-        putenv(buf);
-        // 成功的话调到 新的执行体上
-        execl(path, path, NULL);
+    //     // 添加环境变量,用于当前会话中
+    //     sprintf(buf, "REQUEST_METHOD=%s", method);
+    //     putenv(buf);
+    //     // 继续凑环境变量串,放到当前会话种
+    //     if (strcasecmp(buf, "POST") == 0) {
+    //         sprintf(buf, "CONTENT_LENGTH=%d", contlen);
+    //     }
+    //     putenv(buf);
+    //     // 成功的话调到 新的执行体上
+    //     execl(path, path, NULL);
 
-        // 这行代码原本是不用的, 但是防止 execl执行失败, 子进程没有退出.妙招
-        exit(EXIT_SUCCESS);
-    }
-    // 父进程, 为所欲为了,先发送个OK
-    write(clientFileDesc, "HTTP/1.0 200 OK\r\n", 17);
-    close(pocgi[1]);
-    close(picgi[0]);
+    //     // 这行代码原本是不用的, 但是防止 execl执行失败, 子进程没有退出.妙招
+    //     exit(EXIT_SUCCESS);
+    // }
+    // // 父进程, 为所欲为了,先发送个OK
+    // write(clientFileDesc, "HTTP/1.1 200 OK\r\n", 17);
+    // close(pocgi[1]);
+    // close(picgi[0]);
 
-    if (strcasecmp(method, "POST") == 0) {
-        int i; //将数据都写入到 picgi 管道中, 让子进程在 picgi[0]中读取 =>
-               // STDIN_FILENO
-        for (i = 0; i < contlen; ++i) {
-            read(clientFileDesc, &c, 1);
-            write(picgi[1], &c, 1);
-        }
-    }
-    //从子进程中 读取数据 发送给客户端, 多线程跨进程阻塞模型
-    while (read(pocgi[0], &c, 1) > 0)
-        write(clientFileDesc, &c, 1);
+    // if (strcasecmp(method, "POST") == 0) {
+    //     int i; //将数据都写入到 picgi 管道中, 让子进程在 picgi[0]中读取 =>
+    //            // STDIN_FILENO
+    //     for (i = 0; i < contlen; ++i) {
+    //         read(clientFileDesc, &c, 1);
+    //         write(picgi[1], &c, 1);
+    //     }
+    // }
+    // //从子进程中 读取数据 发送给客户端, 多线程跨进程阻塞模型
+    // while (read(pocgi[0], &c, 1) > 0)
+    //     write(clientFileDesc, &c, 1);
 
-    close(pocgi[0]);
-    close(picgi[1]);
-    //等待子进程结束
-    waitpid(pid, NULL, 0);
+    // close(pocgi[0]);
+    // close(picgi[1]);
+    // //等待子进程结束
+    // waitpid(pid, NULL, 0);
 }
