f12d19610506bd90b83095c5e29fdf9c413c24fa
tracy_2921@yeah.net <tracy_2921@yeah.net>
2017-12-24 12:15:13 +0800

    treeCount

--- src/treeCount/treeCount.cpp
+++ src/treeCount/treeCount.cpp
@@ -1,8 +1,8 @@
 #include "sds.h"
 #include <iostream>
 #include <string>
-#include<vector>
 #include <unordered_map>
+#include <vector>
 
 using std::make_pair;
 using std::string;
@@ -14,18 +14,21 @@ typedef std::unordered_map<std::string, city> tree;
 #define INIT_SIZE 10000
 
 
-char *treeCount(char *path) {
- string s= "",s1 ="";
+string treeCount(char *path) {
+    string s = "{", s1 = "";
     tree t;
-    int count, i=0,j=0;
-    char *cityName, *treeName;
+    int count, i = 0, j = 0,n;
+    char cityName[100], treeName[100];
+    char str1[100], str2[100];
+    memset(str1, 0, 100);
+    memset(str2, 0, 100);
     FILE *fp = fopen(path, "rb");
     while (!feof(fp)) {
-        fscanf(fp, "%s %s %d", cityName, treeName, &count);
+        fscanf(fp, "%s %s %d", treeName, cityName, &count);
         for (tree::iterator it = t.begin(); it != t.end(); it++) {
             if (treeName == (*it).first) {
                 for (city::iterator iter = ((*it).second).begin();
-                     iter != ((*it).second).end(); ((*it).second)++) {
+                     iter != ((*it).second).end(); iter++) {
                     if (cityName == (*iter).first) {
                         (*iter).second += count;
                         i = 1;
@@ -45,41 +48,64 @@ char *treeCount(char *path) {
             t.insert(make_pair(treeName, c));
         }
     }
-    s=str1("{");
-    for (tree::iterator it = t.begin(); it != t.end();it++) {
-            s1.append("\"");
-            s1.append((*it).first);
-            s1.append("\":{");
-        for(city::iterator iter=(*it).second.begin();iter!=(*it).second.end();iter++){
-            s1.append( "\"");
-            s1.append(((*it).second).first);
-            s1.append("\":\"");
-            s1.append(((*it).second).second);
-            s1.append( "\",");
+    for (tree::iterator it = t.begin(); it != t.end(); it++) {
+
+        s1.append("\"");
+        s1.append((*it).first);
+        s1.append("\":[");
+        n=0;
+        for (city::iterator iter = (*it).second.begin();
+             iter != (*it).second.end(); iter++) {
+                 if(n==0){
+               n=1;
+           }else{
+               s1.append(",");
+           }
+                 s1.append("{\"name\":\"");
+            i = 0;
+           
+            while (1) {
+                str1[i] = ((*iter).second % 10) + '0';
+                i++;
+                (*iter).second = (*iter).second / 10;
+                if ((*iter).second == 0)
+                    break;
+            }
+            for (j = 0, i = i - 1; i >= 0; i--, j++) {
+                str2[j] = str1[i];
+            }
+            s1.append((*iter).first);
+            s1.append("\",\"value\":\"");
+            s1.append(str2);
+            s1.append("\"}");
         }
-        s1.append( "},");
+        s1.append("]");
         s.append(s1);
-        s1="";
+        s1 = "";
     }
     s.append("}");
     return s;
 }
 
-
-
+#define TREE_TEST
 #ifdef TREE_TEST
 
 void testTreeCount() {
-    const char *path = "./treeTest.txt";
-    sds *s = treeCount(path);
-    if (sdsCompareStr(("{\"柏树\":[{\"name\":\"沈阳\", "
-                       "\"value\":\"10\"},{\"name\":\"山西\", "
-                       "\"value\":\"15\"}]}") == 0) ||
-        (sdsCompareStr(s, "{\"柏树\":[{\"name\":\"山西\", "
-                          "\"value\":\"15\"},{\"name\":\"沈阳\", "
-                          "\"value\":\"10\"}]}") == 0))
+    char path[] = "./treeTest.txt";
+    string s = treeCount(path);
+    string s1="{\"柏树\":[{\"name\":\"山西\",""\"value\":\"15\"},{\"name\":\"沈阳\",""\"value\":\"10\"}]}";
+    string s2="{\"柏树\":[{\"name\":\"沈阳\",""\"value\":\"10\"},{\"name\":\"山西\",""\"value\":\"15\"}]}";
+    if(s.compare(s1)==0||s.compare(s2)==0){
+        std::cout<<"treeCount success"<<std::endl;
+    }else{
+        std::cout<<"treeCount fail"<<std::endl;
+    }
+
 }
 
-int main() {}
+int main(){
+    testTreeCount() ;
+    return 0;
+}
 
 #endif
